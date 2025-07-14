0a3e46efb808c8cedb3fffd915f86632335ccae1
trivedi089 <satyachbs007@gmail.com>
2025-07-15 00:08:28 +0530

    mapping entities with db

--- src/main/java/com/dailycodebuffer/spring/data/jpa/tutorial/entity/Student.java
+++ src/main/java/com/dailycodebuffer/spring/data/jpa/tutorial/entity/Student.java
@@ -1,7 +1,6 @@
 package com.dailycodebuffer.spring.data.jpa.tutorial.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,12 +11,29 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(
+        name="tbl_student",
+        uniqueConstraints = @UniqueConstraint(
+            name = "emailid_unique",
+            columnNames = "email_address"
+        )
+)
 public class Student {
 
     @Id
+    @SequenceGenerator(
+            name="student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+    generator = "student_sequence")
     private Long studentId;
     private String firstname;
     private String lastName;
+
+    @Column(name="email_address",
+            nullable = false)
     private String emailId;
     private String guardianName;
     private String guardianEmail;
